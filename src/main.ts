7f76f6c602925a073ecd8c9a8d2f1c0d6c3d7f67
Sunny Han <shan66@ucsc.edu>
2024-10-05 17:31:05 -0700

    updating game name

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let numClicks = 0;
+const clickButton = document.createElement("button");
+clickButton.innerHTML = "😼";
+app.append(clickButton);
+clickButton.onclick = () => {
+  numClicks++;
+  clickButton.innerHTML = `😼 ${numClicks}`;
+};
